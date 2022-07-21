96ec362d70543aa49f89290c1abf6591e0f99964
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-07-21 20:46:31 +0200

    testing password stenght

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inpMdp = document.querySelector('.form-groupe:nth-child(3) input');
 const inpConfirme = document.querySelector('.form-groupe:nth-child(4) input');
 const allImg = document.querySelectorAll('.icone-verif');
 const allSpan = document.querySelectorAll('span');
-const allLigne = document.querySelectorAll('.ligne-div');
+const allLigne = document.querySelectorAll('.ligne div');
 
 inputUtilisateur.addEventListener('input', (e) => {
   if (e.target.value.length >= 3) {
@@ -23,8 +23,6 @@ inputUtilisateur.addEventListener('input', (e) => {
 inpMail.addEventListener('input', (e) => {
   const regexEmail = /\S+@\S+\.\S+/;
 
-  console.log(e.target.value.search());
-
   if (e.target.value.search(regexEmail) === 0) {
     allImg[1].style.display = 'inline';
     allImg[1].src = 'ressources/check.svg';
@@ -33,5 +31,85 @@ inpMail.addEventListener('input', (e) => {
     allImg[1].style.display = 'inline';
     allImg[1].src = 'ressources/error.svg';
     allSpan[1].style.display = 'inline';
+  }
+});
+
+// validation creation du mot de passe
+
+let valeurInp;
+
+const specialCar = /[^a-zA-Z0-9]/; // tout les characteres speciaux
+const alphabet = /[a-z]/i; // tout l'alphabet
+const chiffres = /[0-9]/; // tout les chiffres
+
+let objValidation = {
+  symbole: 0,
+  lettre: 0,
+  chiffre: 0,
+};
+
+inpMdp.addEventListener('input', (e) => {
+  valeurInp = e.target.value;
+
+  if (valeurInp.search(specialCar) !== -1) {
+    objValidation.symbole = 1;
+  }
+
+  if (valeurInp.search(alphabet) !== -1) {
+    objValidation.lettre = 1;
+  }
+
+  if (valeurInp.search(chiffres) !== -1) {
+    objValidation.chiffre = 1;
+  }
+
+  // validation input au changement ou suppression
+
+  if ((e.inputType = 'deleteContentBackward')) {
+    if (valeurInp.search(specialCar) === -1) {
+      objValidation.symbole = 0;
+    }
+
+    if (valeurInp.search(alphabet) === -1) {
+      objValidation.lettre = 0;
+    }
+
+    if (valeurInp.search(chiffres) === -1) {
+      objValidation.chiffre = 0;
+    }
+  }
+
+  let testAll = 0;
+
+  for (const property in objValidation) {
+    if (objValidation[property] > 0) {
+      testAll++;
+    }
+    if (testAll < 3) {
+      allSpan[2].style.display = 'inline';
+      allImg[2].style.display = 'inline';
+      allImg[2].src = 'ressources/error.svg';
+    } else {
+      allSpan[2].style.display = 'none';
+      allImg[2].src = 'ressources/check.svg';
+    }
+  }
+
+  if (valeurInp.length <= 6 && valeurInp.length > 0) {
+    allLigne[0].style.display = 'block';
+    allLigne[1].style.display = 'none';
+    allLigne[2].style.display = 'none';
+  } else if (valeurInp.length > 6 && valeurInp.length <= 9) {
+    allLigne[0].style.display = 'block';
+    allLigne[1].style.display = 'block';
+    allLigne[2].style.display = 'none';
+  } else if (valeurInp.length > 9) {
+    allLigne[0].style.display = 'block';
+    allLigne[1].style.display = 'block';
+    allLigne[2].style.display = 'block';
+  } else if (valeurInp.length === 0) {
+    allLigne[0].style.display = 'none';
+    allLigne[1].style.display = 'none';
+    allLigne[2].style.display = 'none';
   }
 });
